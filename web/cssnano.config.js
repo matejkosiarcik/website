cb54693b0c13d6507cc77cf1bb7e008213fb4a4d
Matej Košiarčik <matej.kosiarcik@gmail.com>
2025-05-04 20:34:22 +0200

    chore: remove old website files

--- web/cssnano.config.js
+++
@@ -1,7 +0,0 @@
-const preset = require('cssnano-preset-default');
-
-module.exports = preset({
-  discardComments: {
-    removeAll: true,
-  },
-});
--- web/script/main.ts
+++
@@ -1,6 +0,0 @@
-// Must load enable-js first, otherwise there be dragons
-import './enable-js';
-
-import './redirect';
-import './header';
-import 'instant.page';
